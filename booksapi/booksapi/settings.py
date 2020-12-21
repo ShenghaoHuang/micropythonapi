399ef632effc5022971b8145df72e61c409af6e7
Shenghao <itzshengh95@gmail.com>
2020-12-21 13:10:43 -0500

    Testing django_on_heroku

--- booksapi/booksapi/settings.py
+++ booksapi/booksapi/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import django_heroku
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +124,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+django_on_heroku.settings(locals())
