806d41a0d649dcf47b04dab2df155f52fba58f12
Shenghao <itzshengh95@gmail.com>
2020-12-22 17:25:56 -0500

    Commented out Static Dir

--- booksapi/booksapi/settings.py
+++ booksapi/booksapi/settings.py
@@ -122,9 +122,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 django_on_heroku.settings(locals())
 
 options = DATABASES['default'].get('OPTIONS', {})
