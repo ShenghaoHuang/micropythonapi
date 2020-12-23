5d7de161918ad18b1f7e5e673786fa0c87d74d0a
Shenghao <itzshengh95@gmail.com>
2020-12-23 12:03:41 -0500

    Changed load_csv

--- booksapi/books/load_csv.py
+++ booksapi/books/load_csv.py
@@ -2,20 +2,21 @@ import os
 import csv
 from .models import Book
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path = os.path.join(BASE_DIR, '/booksapi/staticfiles/bestsellers-with-categories.csv')
-#path = 'https://github.com/ShenghaoHuang/micropythonapi/blob/main/bestsellers-with-categories.csv'
-#path = 'bestsellers-with-categories.csv'
-with open(path) as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    for row in reader:
-        _, created = Book.objects.get_or_create(
-            name=row[0],
-            author=row[1],
-            rating=row[2],
-            reviews=row[3],
-            price=row[4],
-            year=row[5],
-            genre=row[6]
-        )
+if __name__ == '__main__':
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    #path = os.path.join(BASE_DIR, '/booksapi/staticfiles/bestsellers-with-categories.csv')
+    #path = 'https://github.com/ShenghaoHuang/micropythonapi/blob/main/bestsellers-with-categories.csv'
+    path = 'bestsellers-with-categories.csv'
+    with open(path) as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        for row in reader:
+            _, created = Book.objects.get_or_create(
+                name=row[0],
+                author=row[1],
+                rating=row[2],
+                reviews=row[3],
+                price=row[4],
+                year=row[5],
+                genre=row[6]
+            )
