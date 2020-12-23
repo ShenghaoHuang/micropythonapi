78af7dd49076579719845eb02522741f1d17b812
Shenghao <itzshengh95@gmail.com>
2020-12-23 12:50:51 -0500

    Changed load_csv

--- booksapi/books/models.py
+++ booksapi/books/models.py
@@ -14,21 +14,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
-
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#path = os.path.join(BASE_DIR, '/booksapi/staticfiles/bestsellers-with-categories.csv')
-#path = 'https://github.com/ShenghaoHuang/micropythonapi/blob/main/bestsellers-with-categories.csv'
-path = 'bestsellers-with-categories.csv'
-with open(path) as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    for row in reader:
-        _, created = Book.objects.get_or_create(
-            name=row[0],
-            author=row[1],
-            rating=row[2],
-            reviews=row[3],
-            price=row[4],
-            year=row[5],
-            genre=row[6]
-        )
