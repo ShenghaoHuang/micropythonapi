b273a16abc6614b6849be54cf514ea0051448e71
Shenghao <itzshengh95@gmail.com>
2020-12-26 15:41:30 -0500

    Added Command Class

--- booksapi/books/management/commands/load_csv.py
+++ booksapi/books/management/commands/load_csv.py
@@ -1,23 +1,28 @@
 import os.path
 import csv
 from ...models import Book
+from django.core.management.base import BaseCommand
 
 #BASE_DIR = os.path.dirname('micropythonapi')
-BASE_DIR = os.path.dirname(os.path.abspath('micropythonapi'))
-path = (BASE_DIR +'/booksapi/staticfiles/bestsellers-with-categories.csv')
+#BASE_DIR = os.path.dirname(os.path.abspath('micropythonapi'))
+#path = (BASE_DIR +'/booksapi/staticfiles/bestsellers-with-categories.csv')
 # path = 'https://github.com/ShenghaoHuang/micropythonapi/blob/main/bestsellers-with-categories.csv'
 # path = 'bestsellers-with-categories.csv'
+class Command(BaseCommand):
 
-with open(path) as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    for row in reader:
-        _, created = Book.objects.get_or_create(
-            name=row[0],
-            author=row[1],
-            rating=row[2],
-            reviews=row[3],
-            price=row[4],
-            year=row[5],
-            genre=row[6]
-        )
+    def handle(self, *args, **options):
+        BASE_DIR = os.path.dirname(os.path.abspath('micropythonapi'))
+        path = (BASE_DIR + '/booksapi/staticfiles/bestsellers-with-categories.csv')
+        with open(path) as f:
+            reader = csv.reader(f)
+            next(reader, None)
+            for row in reader:
+                _, created = Book.objects.get_or_create(
+                    name=row[0],
+                    author=row[1],
+                    rating=row[2],
+                    reviews=row[3],
+                    price=row[4],
+                    year=row[5],
+                    genre=row[6]
+                )
